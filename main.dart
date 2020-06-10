24ffcac645b18216e9c4386bb2003a376351c4a5
MAC MAC <pipermichael@aol.com>
2020-06-10 09:50:30 +0100

    first commit

--- main.dart
+++ main.dart
@@ -18,3 +18,18 @@ class MyApp extends StatelessWidget {
     return screen(context);
   }
 }
+
+
+//http: ^0.12.0+4
+//provider: ^4.0.2
+//localstorage: ^3.0.0
+//image_picker: ^0.6.5+1
+//shared_preferences: ^0.5.6
+//flutter_paystack: ^1.0.3+1
+//google_maps_webservice: ^0.0.16
+//google_fonts: ^0.3.10
+//flutter_dialogflow: ^0.1.3
+//carousel_slider: ^1.4.1
+//url_launcher: ^5.4.2
+//curved_navigation_bar: ^0.3.2
+//flutter_statusbarcolor: ^0.2.3
